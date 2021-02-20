3ef2243670f91b2d1ff407e0d5d061fcea00ec92
r666666 <evrikaa6@gmail.com>
2021-02-20 20:16:24 +0200

    add navbar navigation

--- frontend_service/src/components/Navbar/index.js
+++ frontend_service/src/components/Navbar/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './style.scss';
-
 import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+import { links } from '../../utils/constants';
 
 function Navbar(props) {
   return (
     <span className="navbar">
-      <Menu fluid widths={2}>
+      <Menu fluid>
         <Menu.Item position="left">
           <div className="logo">Pet Finder</div>
+        </Menu.Item>
+        <Menu.Item>
+          <div className="tabs-wrap">
+            <Link to={{ pathname: links.homepage }}>
+              <div className="tab-item active">Пошук</div>
+            </Link>
+            <Link to={{ pathname: links.createAd }}>
+              <div className="tab-item">Загубив</div>
+            </Link>
+            <Link to={{ pathname: links.createAd }}>
+              <div className={'tab-item'}>Знайшов</div>
+            </Link>
+          </div>
         </Menu.Item>
         <Menu.Item position="right">
           <div className="user-action-wrap">Account</div>
