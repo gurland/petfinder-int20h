c9ee71e2eea3ea6d22181cec4de5da19b96fcf96
r666666 <evrikaa6@gmail.com>
2021-02-20 16:17:52 +0200

    add homepage

--- frontend_service/src/pages/Homepage/index.js
+++ frontend_service/src/pages/Homepage/index.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react';
-
 import './style.scss';
+
+import { Input } from 'semantic-ui-react';
 
 function Homepage() {
   return (
-    <>
-      <div className="main-content">homepage</div>
-    </>
+    <div className="main-content">
+      <div className="inputs-wrap">
+        <Input placeholder="Search" />
+        <div className="map">Map</div>
+      </div>
+    </div>
   );
 }
 
