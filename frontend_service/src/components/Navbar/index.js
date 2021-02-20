ad7a5c35267a40b19985b02d3cd467a887c9a4dd
dvoyakiy <dvoyakiyyy@gmail.com>
2021-02-20 19:49:09 +0200

    Add link to homepage

--- frontend_service/src/components/Navbar/index.js
+++ frontend_service/src/components/Navbar/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './style.scss';
 
 import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { links } from '../../utils/constants';
 
 function Navbar(props) {
   return (
     <span className="navbar">
       <Menu fluid widths={2}>
         <Menu.Item position="left">
-          <div className="logo">Pet Finder</div>
+          <Link className="logo" to={links.homepage}>
+            Pet Finder
+          </Link>
         </Menu.Item>
         <Menu.Item position="right">
           <div className="user-action-wrap">Account</div>
