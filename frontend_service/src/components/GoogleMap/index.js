5da233b98d6bdee43e99bb34759d3e0a2f0f7463
dvoyakiy <dvoyakiyyy@gmail.com>
2021-02-20 19:22:04 +0200

    Refactor Google Map component

--- frontend_service/src/components/GoogleMap/index.js
+++ frontend_service/src/components/GoogleMap/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import GoogleMapReact from 'google-map-react';
 
-function GoogleMap({ circleRadius, circleCenter }) {
+function GoogleMap({ onClick, mapRef, mapsRef }) {
   const kyivCenter = { lat: 50.450001, lng: 30.523333 };
 
   return (
@@ -11,22 +11,25 @@ function GoogleMap({ circleRadius, circleCenter }) {
       bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_TOKEN, libraries: 'places' }}
       defaultCenter={kyivCenter}
       defaultZoom={10}
-      onGoogleApiLoaded={({ map, maps }) =>
-        new maps.Circle({
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.3,
-          map,
-          center: kyivCenter,
-          radius: 275,
-        })
-      }
+      onClick={onClick}
+      onGoogleApiLoaded={({ map, maps }) => {
+        if (mapRef) mapRef.current = map;
+        if (mapsRef) mapsRef.current = maps;
+      }}
     />
   );
 }
 
-GoogleMap.propTypes = {};
+GoogleMap.propTypes = {
+  onClick: PropTypes.func,
+  mapRef: PropTypes.object,
+  mapsRef: PropTypes.object,
+};
+
+GoogleMap.defaultProps = {
+  onClick: () => {},
+  mapRef: null,
+  mapsRef: null,
+};
 
 export default GoogleMap;
