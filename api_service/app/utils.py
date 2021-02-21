470e2e7fa85412e5d9ab43a3d6225908b3b071b2
Stanislav Bobokalo <stas.gurland@gmail.com>
2021-02-21 02:22:07 +0200

    Fix jpg

--- api_service/app/utils.py
+++ api_service/app/utils.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 
 def store_b64_image_to_disk(b64string):
     image_contents = b64decode(b64string)
-    filename = join(MEDIA_PATH, f"{uuid4().jpg}")
+    filename = join(MEDIA_PATH, f"{uuid4()}.jpg")
     with open(filename, 'wb') as img_file:
         img_file.write(image_contents)
 
